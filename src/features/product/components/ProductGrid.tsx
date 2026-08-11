ec8fc9f123c52a63f99004314feb5325639ddc52
Anthony <150530087+codimaniac@users.noreply.github.com>
2026-08-11 18:58:06 +0100

    fix: minor fix

--- src/features/product/components/ProductGrid.tsx
+++ src/features/product/components/ProductGrid.tsx
@@ -1,6 +1,6 @@
 import cn from "@/lib/cn";
 
-const ProductGrid = ({children, className}: {children: React.ReactNode, className: string}) => {
+const ProductGrid = ({children, className}: {children: React.ReactNode, className?: string}) => {
   return (
     <div className={cn("grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3", className)}>
         {children}
